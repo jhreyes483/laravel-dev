6d6eb3155e15096f1da2795984c8c4929d8df1a7
jhreyes483 <jhreyes483@misena.edu.co>
2022-04-16 14:22:01 -0500

    config to login view

--- resources/views/modules/auth/form_login.blade.php
+++ resources/views/modules/auth/form_login.blade.php
@@ -5,8 +5,10 @@
                 <input id="tab-1" type="radio" name="tab" class="sign-in" checked=""><label for="tab-1" class="tab">Iniciar Sesion</label>
                 <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Bienvenido</label>
                 <div class="login-form">
-                <form method="post" action="http://larave-developer.herokuapp.com/auth">
-                    <input type="hidden" name="_token" value="h0nLeOaU8BqtkQ3HNrzkiHmfEFR8LYgHPNnwHDx7">                    <div class="sign-in-htm">
+                <form method="post" action="{{ route('login.auth') }}">
+                    @csrf
+
+                     <div class="sign-in-htm">
                         <div class="group">
                             <label for="id_admin" class="label">Identificacion</label>
                             <input id="id_admin" type="text" name="id_admin" class="input">
